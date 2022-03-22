4815b524e91f32ecdd5582016d85c4c8dc74ddd0
Joe Curtis <joecurtis9999@gmail.com>
2022-03-22 08:53:07 +0000

    media queries

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ const hideMobileMenu = () => {
     const menuBars = document.querySelector('is-active')
     if(window.innerWidth <= 768 && menuBars) {
         menu.classList.toggle('is-active')
-        menuLinks.classList.remove('is-active')
+        menuLinks.classList.remove('active')
     }
 };
 
